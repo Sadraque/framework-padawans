53c7f0c843094d936ba45b58cfeae6e953ac7800
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-02-27 16:52:54 -0300

    projeto inicial

---
+++ src/test/java/com/padawans/framework/FrameworkPadawansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padawans.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkPadawansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
