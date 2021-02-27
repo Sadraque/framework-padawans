a6a6bd93436748c72c8af7bbd0fd85a13dc1ae34
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-02-27 19:33:03 -0300

    criando classe Album

---
+++ src/main/java/com/padawans/framework/model/Album.java
@@ -0,0 +1,19 @@
+package com.padawans.framework.model;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@EqualsAndHashCode @NoArgsConstructor
+public class Album implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer userId;
+	private Integer id;
+	private String title;
+
+}
