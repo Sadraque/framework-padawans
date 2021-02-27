05ea95a7e4c927919e6352f92bef83e63d149bc3
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-02-27 19:35:53 -0300

    criando classe Todo

---
+++ src/main/java/com/padawans/framework/model/Todo.java
@@ -0,0 +1,20 @@
+package com.padawans.framework.model;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@EqualsAndHashCode @NoArgsConstructor
+public class Todo implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer userId;
+	private Integer id;
+	private String title;
+	private boolean completed;
+
+}
