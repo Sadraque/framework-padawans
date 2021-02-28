a84eb98754214a1b1adb87408f42866f6028b1a4
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-02-28 12:08:35 -0300

    testando index controller

--- src/main/java/com/padawans/framework/FrameworkPadawansApplication.java
+++ src/main/java/com/padawans/framework/FrameworkPadawansApplication.java
@@ -2,8 +2,10 @@ package com.padawans.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class FrameworkPadawansApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/padawans/framework/controller/IndexController.java
+++ src/main/java/com/padawans/framework/controller/IndexController.java
@@ -1,20 +1,16 @@
 package com.padawans.framework.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.padawans.framework.services.PostService;
 
 @Controller
+@ComponentScan(basePackages = "com.padawans.framework.controller")
 public class IndexController {
-
-	@Autowired
-	PostService postService;
 
 	@RequestMapping("/")
 	public String index() {
-		postService.getPosts();
 
-		return "index";
+		return "index.html";
 	}
 }
