cc29444f04a82be3452b483dc9bc2949c6726be6
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-02-27 22:29:21 -0300

    realizando testes com as classes do pacote SERVICE

--- src/main/java/com/padawans/framework/configuration/ApiConfiguration.java
+++ src/main/java/com/padawans/framework/configuration/ApiConfiguration.java
@@ -4,15 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @PropertySource("classpath:application.properties")
+@Getter @Setter
 public class ApiConfiguration {
 
 	@Value("${api.resource.baseUrl}")
 	public String apiBaseUrl;
 
 	@Value("${api.resource.albuns}")
-	public String apiAlbuns;
+	public String apiAlbums;
 
 	@Value("${api.resource.posts}")
 	public String apiPosts;
--- src/main/java/com/padawans/framework/controller/IndexController.java
+++ src/main/java/com/padawans/framework/controller/IndexController.java
@@ -1,14 +1,20 @@
 package com.padawans.framework.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.padawans.framework.services.PostService;
 
 @Controller
 public class IndexController {
 
+	@Autowired
+	PostService postService;
+
 	@RequestMapping("/")
 	public String index() {
-		
+		postService.getPosts();
+
 		return "index";
 	}
 }
